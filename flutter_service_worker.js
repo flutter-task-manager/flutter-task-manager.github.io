e18bab352a63b58bfd2882371922fbbc5dcf43a7
cezres <cezresx@gmail.com>
2024-02-05 07:42:12 +0000

    Update files

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,8 +12,8 @@ const RESOURCES = {"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57
 "assets/fonts/MaterialIcons-Regular.otf": "58ca9b70459d21ab2ffb2fde3f0929d4",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "manifest.json": "0867c3e13649ac4d06fe34b7b3ddce08",
-"index.html": "7a11ff505bcc2a863631713e7623e63e",
-"/": "7a11ff505bcc2a863631713e7623e63e",
+"index.html": "5f3d0c66b58d72dcc666ba141c2d4b23",
+"/": "5f3d0c66b58d72dcc666ba141c2d4b23",
 "canvaskit/chromium/canvaskit.wasm": "143af6ff368f9cd21c863bfa4274c406",
 "canvaskit/chromium/canvaskit.js": "0ae8bbcc58155679458a0f7a00f66873",
 "canvaskit/skwasm.wasm": "2fc47c0a0c3c7af8542b601634fe9674",
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "fedb190d252fae2dbf412d9792b25993",
+"main.dart.js": "e844cc11a98c73851d482b136a8540ca",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "version.json": "ff966ab969ba381b900e61629bfb9789"};
 // The application shell files that are downloaded before a service worker can
